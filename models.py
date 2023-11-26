5a9adfd73b6fcfcfcc36db42112c9d93a51a6017
jwelkener <56115991+jwelkener@users.noreply.github.com>
2023-11-25 20:09:07 -0900

    user flows and models

--- models.py
+++ models.py
@@ -1,1 +1,33 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = "https://unsplash.com/photos/a-dark-blue-area-with-a-white-stripe-on-it-wmyE5IBiOmo"
+
+class User(db.Model):
+	"""Site user."""
+
+	__tablename__ = "users"
+
+	id = db.Column(db.Integer, primary_key=True)
+	first_name = db.Column(db.Text, nullable=False)
+	last_name = db.Column(db.Text, nullable=False)
+	image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
+
+	@property
+	def full_name(self):
+		"""Return full name of user."""
+
+		return f"{self.first_name}{self.last_name}"
+	
+	def connect_db(app):
+		"""Connect this database to provided Flask app. 
+		
+		This should be called in Flask app."""
+
+		db.app = app
+		db.init_app(app)
+
+	
